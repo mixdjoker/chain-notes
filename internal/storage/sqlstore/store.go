3de92d2502b61c7af0f9d1402e1ec00afaaca6e3
Mikhail Orlov <mix.djoker@gmail.com>
2025-06-25 21:44:02 +0200

    add: some fixes in storage logic

--- internal/storage/sqlstore/store.go
+++ internal/storage/sqlstore/store.go
@@ -27,12 +27,6 @@ type Store struct {
 	db *sql.DB
 }
 
-// Query represents a SQL query with its name and raw SQL string
-type Query struct {
-	Name     string
-	QueryRaw string
-}
-
 func New(db *sql.DB) *Store {
 	return &Store{db: db}
 }
@@ -43,9 +37,7 @@ func (s *Store) ParentExists(ctx context.Context, hash string) (bool, error) {
 		return true, nil // root commit
 	}
 
-	q := Query{
-		Name: "sqlstore.ParentExists",
-	}
+	queryName := "sqlstore.ParentExists"
 
 	query, args, err := sq.
 		Select("1").
@@ -55,17 +47,15 @@ func (s *Store) ParentExists(ctx context.Context, hash string) (bool, error) {
 		PlaceholderFormat(sq.Dollar).
 		ToSql()
 	if err != nil {
-		return false, fmt.Errorf("[sqlstore] failed to build SQL query %s: %w", q.Name, err)
+		return false, fmt.Errorf("[sqlstore] failed to build SQL query %s: %w", queryName, err)
 	}
 
-	q.QueryRaw = query
-
 	var dummy int
-	err = s.db.QueryRowContext(ctx, q.QueryRaw, args...).Scan(&dummy)
+	err = s.db.QueryRowContext(ctx, query, args...).Scan(&dummy)
 	if errors.Is(err, sql.ErrNoRows) {
 		return false, nil // parent does not exist
 	} else if err != nil {
-		return false, fmt.Errorf("[sqlstore] failed to execute query %s: %w", q.Name, err)
+		return false, fmt.Errorf("[sqlstore] failed to execute query %s: %w", queryName, err)
 	}
 
 	return true, nil
@@ -73,20 +63,40 @@ func (s *Store) ParentExists(ctx context.Context, hash string) (bool, error) {
 
 // InsertCommit inserts a new commit into the database
 func (s *Store) InsertCommit(ctx context.Context, hash string, input *commitservice.CommitInput) error {
-	_, err := s.db.ExecContext(ctx, `
-		INSERT INTO commits (hash, parent_hash, tree_hash, timestamp, author_pubkey, signature, message)
-		VALUES ($1, $2, $3, $4, $5, $6, $7)
-	`,
-		hash,
-		input.ParentHash,
-		input.TreeHash,
-		input.Timestamp,
-		input.AuthorPubKey,
-		input.Signature,
-		input.Message,
-	)
-	if err != nil {
-		return errors.New("failed to insert commit: " + err.Error())
+	queryName := "sqlstore.InsertCommit"
+
+	commitColums := []string{
+		colHash,
+		colParentHash,
+		colTreeHash,
+		colTimestamp,
+		colAuthorPubKey,
+		colSignature,
+		colMessage,
 	}
+
+	query, args, err := sq.
+		Insert(tableCommits).
+		Columns(commitColums...).
+		Values(
+			hash,
+			input.ParentHash,
+			input.TreeHash,
+			input.Timestamp,
+			input.AuthorPubKey,
+			input.Signature,
+			input.Message,
+		).
+		PlaceholderFormat(sq.Dollar).
+		ToSql()
+	if err != nil {
+		return fmt.Errorf("[sqlstore] failed to build SQL query %s: %w", queryName, err)
+	}
+
+	_, err = s.db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return fmt.Errorf("[sqlstore] failed to insert commit %s: %w", queryName, err)
+	}
+
 	return nil
 }
