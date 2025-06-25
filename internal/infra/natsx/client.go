d2fff960bd55987364fd7711b43aa52e9f5a0d70
Mikhail Orlov <mix.djoker@gmail.com>
2025-06-25 03:57:46 +0200

    update natsx

--- internal/infra/natsx/client.go
+++ internal/infra/natsx/client.go
@@ -2,25 +2,34 @@ package natsx
 
 import (
 	"log"
+	"time"
 
 	"github.com/nats-io/nats.go"
 )
 
 // Connect establishes a connection to a NATS server using the provided URL.
 func Connect(url string) (*nats.Conn, error) {
-	nc, err := nats.Connect(url)
+	opts := []nats.Option{
+		nats.Name("ChainNotes-NATS"),
+		nats.MaxReconnects(10),              // До 10 попыток переподключения
+		nats.ReconnectWait(2 * time.Second), // Интервал между попытками
+		nats.Timeout(5 * time.Second),       // Таймаут на попытку соединения
+		nats.RetryOnFailedConnect(true),     // Включает ретраи при первом подключении
+		nats.ReconnectHandler(func(_ *nats.Conn) {
+			log.Println("[natsx] reconnected to NATS")
+		}),
+		nats.ClosedHandler(func(_ *nats.Conn) {
+			log.Println("[natsx] connection closed")
+		}),
+		nats.DisconnectErrHandler(func(_ *nats.Conn, err error) {
+			log.Printf("[natsx] disconnected due to: %v", err)
+		}),
+	}
+
+	nc, err := nats.Connect(url, opts...)
 	if err != nil {
 		return nil, err
 	}
-
-	// Optionally set up a connection close handler
-	nc.SetClosedHandler(func(_ *nats.Conn) {
-		log.Println("[natsx] connection closed")
-	})
-
-	nc.SetReconnectHandler(func(_ *nats.Conn) {
-		log.Println("[natsx] reconnected to NATS")
-	})
 
 	return nc, nil
 }
